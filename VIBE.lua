32e40a5e426af8aa7613c5d6f2f9f2037a8eeb0a
noobregg123 <90770465+noobregg123@users.noreply.github.com>
2021-12-29 19:56:55 +0700

    Create VIBE.lua

---
+++ VIBE.lua
@@ -0,0 +1,31 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("VIBELOCK", "Synapse")
+
+
+local Tab = Window:NewTab("Da Hood")
+local Section = Tab:NewSection("LOCK")
+
+Section:NewButton("BeamedWare.cc", "By  By Elias.#4444", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EliasAtto1/BeamedWare/main/BeamedWare", true))()
+end)
+
+--Couter Bloxx
+
+local Tab = Window:NewTab("Couter blox & Arsenal")
+local Section = Tab:NewSection("LOCK UI")
+
+Section:NewButton("Owl | Hub", "By Youtube", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SiLeNSwOrD/OwlHub/master/OwlHub.txt"))()
+end)
+
+
+local Tab = Window:NewTab("ADMIN COMMAND")
+local Section = Tab:NewSection("infiniteyield & Reviz Admin")
+
+Section:NewButton("infiniteyield", "By IDK", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+end)
+
+Section:NewButton("Reviz Admin", "By IDK", function()
+    loadstring(game:HttpGet(('https://pastebin.com/raw/Caniwq2N'),true))()
+end)
