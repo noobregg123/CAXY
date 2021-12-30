aacca6346e63ab2538ca16b549510a95a215520f
noobregg123 <90770465+noobregg123@users.noreply.github.com>
2021-12-30 11:58:23 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("VIBEHACK", "Synapse")
+local Window = Library.CreateLib("VIBESCRIPT", "Ocean")
 
 
 local Tab = Window:NewTab("Da Hood")
@@ -36,5 +36,12 @@ local Section = Tab:NewSection("ANY SCRIPT")
 
 Section:NewButton("MAKURO | HUB", "By IDK", function()
     loadstring(game:HttpGet"https://raw.githubusercontent.com/xDepressionx/Free-Script/main/BloxFruit.lua")()
+end)
 
+
+local Tab = Window:NewTab("RO-GHOUL")
+local Section = Tab:NewSection("HACK UI")
+
+Section:NewButton("Ro-Ghoul Ui", "By CAXY COPY", function()
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/z4gs/scripts/master/Ro-Ghoul%20Auto%20Farm.lua"))()
 end)
